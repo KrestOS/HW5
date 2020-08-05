509ba83a01036b7b30d011659ffdd3861034baa2
[KrestOS] <[krestik9@yandex.ru]>
2020-08-05 18:35:58 +0300

    hw6_2

--- src/HW6/Animals/Animals.java
+++ src/HW6/Animals/Animals.java
@@ -1,4 +1,16 @@
 package HW6.Animals;
 
-public class Animals {
+public abstract class Animals {
+
+    String name;
+    int limitRun = 0;
+    int limitSwim = 0;
+
+    public Animals() {
+
+    }
+
+    public abstract void Run(int distance);
+    public abstract void Swim(int distance);
+
 }
--- src/HW6/Animals/Cat.java
+++ src/HW6/Animals/Cat.java
@@ -1,4 +1,31 @@
 package HW6.Animals;
 
 public class Cat extends Animals {
+
+    public Cat(String name) {
+
+        this.name = name;
+
+    }
+
+    @Override
+    public void Run(int distance) {
+        limitRun = 200;
+        if (distance <= limitRun){
+
+            System.out.printf("Кошка %s пробежала %d метров\n", name, distance);
+
+        }else {
+
+            System.out.printf("Кошка %s не сможет пробежать больше %d м !\n",name, limitRun);
+
+        }
+    }
+
+    @Override
+    public void Swim(int distance) {
+
+        System.out.printf("Кошка %s не умеет плавать ):\n",name);
+
+    }
 }
